5f5a03ac919a832126fc2e1753e8f0b5a523678b
Dmitriy Sharandikov <custadialship@gmail.com>
2022-08-02 13:13:45 +0300

    Initial commit

---
+++ src/main/java/com/skyshard/subman/SubmanApplication.java
@@ -0,0 +1,13 @@
+package com.skyshard.subman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmanApplication.class, args);
+    }
+
+}
